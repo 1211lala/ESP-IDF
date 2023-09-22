40b89b743175105329c744fe6cc98d805af5aa83
liuao <2494210546@qq.com>
2023-09-22 21:20:06 +0800

    commit

--- ESP32-S3-Template/main/main.c
+++ ESP32-S3-Template/main/main.c
@@ -1,6 +1,14 @@
+/*
+ * @Author: liuao 2494210546@qq.com
+ * @Date: 2023-09-22 21:11:11
+ * @LastEditors: liuao 2494210546@qq.com
+ * @LastEditTime: 2023-09-22 21:19:23
+ * @FilePath: \ESP32-S3-Template\main\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 #include <stdio.h>
-
+#include <string.h>
 void app_main(void)
 {
 
